9ba6dc216666d2aad280e7ade30bd02784bb5f23
davprog9 <david.arzm9@gmail.com>
2026-05-24 20:15:21 -0400

    Chunking completed

---
+++ app/chunking.py
@@ -0,0 +1,23 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+
+
+def chunking(documents) -> list:
+    """
+    Document chunking function
+
+    Parameters:
+        documents (list): List of Document objects
+    """
+    
+    c_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 300,
+        chunk_overlap = 50,
+        separators = ["\n\n", "\n", " ", ""]  # fallback levels: paragraphs, lines, words, characters
+    )
+
+    splitted_docs = c_splitter.split_documents(documents)
+
+    print("Documents splitting completed")
+    print("Total Documents: " + str(len(documents)))
+    print("Total Chunks: " + str(len(splitted_docs)))
+    
